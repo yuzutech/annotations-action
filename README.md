bc20c8758c97a3f4cd9dd8ba5f2e571fdf1734e3
Guillaume Grossetie <ggrossetie@gmail.com>
2020-10-15 17:50:28 +0200

    Up-to-date project

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-<p align="center">
-  <a href="https://github.com/ssboisen/test-annotations-action"><img alt="GitHub Actions status" src="https://github.com/Mogztter/annotations-action/workflows/test-local/badge.svg"></a>
-</p>
-
 # Annotations Action
+
+[![GitHub Action badge](https://github.com/yuzutech/annotations-action/workflows/test-local/badge.svg)](https://github.com/yuzutech/annotations-action/actions?query=workflow%3Atest-local)
 
 This action creates annotations from a JSON file.
 
@@ -14,13 +12,19 @@ In order to use this action, you will need to generate a JSON file using the fol
     file: "path/to/file.js",
     line: 5,
     message: "my message",
-    title: "title for my annotation"
+    title: "title for my annotation",
+    annotation_level: "failure"
   }
 ]
 ```
 
 ## Inputs
 
+### `repo-token`
+
+**Required** Path to a JSON file which contains a list of annotations
+  
+  
 ### `input`
 
 **Required** Path to a JSON file which contains a list of annotations
@@ -28,16 +32,10 @@ In order to use this action, you will need to generate a JSON file using the fol
 ## Example usage
 
 ```yml
-name: workflow
-on: [push]
-jobs:
-  job:
-    runs-on: ubuntu-18.04
-    steps:
-      - uses: actions/checkout@v1
-      - name: Annotate
-        uses: yuzutech/annotations-action@v0.1.0
-        with:
-          input: './annotations.json'
+- name: Annotate
+uses: yuzutech/annotations-action@v0.1.0
+with:
+  repo-token: "${{ secrets.GITHUB_TOKEN }}"
+  input: './annotations.json'
 ```
 
--- index.js
+++ index.js
@@ -1,69 +1,75 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const fs = require('fs').promises;
+const core = require('@actions/core')
+const github = require('@actions/github')
+const fs = require('fs').promises
 
+const batchIt = (size, inputs) => inputs.reduce((batches, input) => {
+  const current = batches[batches.length - 1]
 
-async function run() {
-  try { 
-    const ghToken = core.getInput("githubToken");
-    const octokit = new github.GitHub(ghToken);
+  current.push(input)
 
-    const reportPath = core.getInput('reportPath');
-    const ref = github.context.sha;
-    const owner = github.context.payload.repository.owner.name;
-    const repo = github.context.payload.repository.name;
-    const workflow = github.context.workflow;
-    const check_run = process.env.GITHUB_WORKFLOW;
+  if (current.length === size) {
+    batches.push([])
+  }
 
-    const reportContent = await fs.readFile(reportPath, 'utf8');
-    const reports = JSON.parse(reportContent);
+  return batches
+}, [[]])
 
-    const { data: { check_runs: [{ id: check_run_id }] } } = await octokit.checks.listForRef({
-        owner,
-        repo,
-        ref,
-        check_run,
-        status: "in_progress"
-    });
+const ANNOTATION_LEVELS = ['notice', 'warning', 'failure']
 
-    //The Github Checks API requires that Annotations are not submitted in batches of more than 50
-    const batchedReports = batchIt(50, reports);
+async function run () {
+  try {
+    const repoToken = core.getInput('repo-token')
+    const octokit = new github.GitHub(repoToken)
 
-    batchedReports.forEach(async (reports) => {
+    const inputPath = core.getInput('input')
+    const ref = github.context.sha
+    const owner = github.context.payload.repository.owner.name
+    const repo = github.context.payload.repository.name
+    const workflow = github.context.workflow
+    const checkRun = process.env.GITHUB_WORKFLOW
 
-      const annotations = reports.map(r => ({ 
-        path: r.file, 
-        start_line: r.line, 
-        end_line: r.line, 
-        annotation_level: "failure", 
-        message: r.message,
-        title: r.title
-      }));
+    const inputContent = await fs.readFile(inputPath, 'utf8')
+    const annotations = JSON.parse(inputContent)
+
+    const { data: { check_runs: [{ id: checkRunId }] } } = await octokit.checks.listForRef({
+      owner,
+      repo,
+      ref,
+      check_run: checkRun,
+      status: 'in_progress'
+    })
+
+    // The GitHub API requires that annotations are submitted in batches of 50 elements maximum
+    const batchedAnnotations = batchIt(50, annotations)
+
+    for (const batch of batchedAnnotations) {
+      const annotations = batch.map(annotation => {
+        let annotationLevel
+        if (ANNOTATION_LEVELS.includes(annotation.annotation_level)) {
+          annotationLevel = annotation.annotation_level
+        } else {
+          annotationLevel = 'failure'
+        }
+        return {
+          path: annotation.file,
+          start_line: annotation.line,
+          end_line: annotation.line,
+          annotation_level: annotationLevel,
+          message: annotation.message,
+          title: annotation.title
+        }
+      })
 
       await octokit.checks.update({
         owner,
         repo,
-        check_run_id,
+        check_run_id: checkRunId,
         output: { title: `${workflow} Check Run`, summary: `${annotations.length} errors(s) found`, annotations }
-      });
-    });
-  } 
-  catch (error) {
-    core.setFailed(error.message);
+      })
+    }
+  } catch (error) {
+    core.setFailed(error.message)
   }
 }
 
 run()
-
-
-const batchIt = (size, inputs) => inputs.reduce((batches, input) => {
-  const current = batches[batches.length - 1];
-
-  current.push(input);
-
-  if (current.length == size) {
-    batches.push([]);
-  }
-
-  return batches;
-}, [[]]);
